6229d87c59a7e94185ab32547a8965558b23f436
Alvaro Garcia <derecerca@gmail.com>
2015-08-19 20:17:47 +0200

    refactor: extract functions from countPalindromes

--- palindrome.hs
+++ palindrome.hs
@@ -8,12 +8,17 @@ palindrome x = if x == (reverse x)
 			   else Nothing
 
 countPalindromes :: String -> Int
-countPalindromes x = (length 
-					 (filter isJust 
+countPalindromes x = (length (generatePalindromes x)) -1 + (length x)
+
+generatePalindromes :: String -> [String]
+generatePalindromes x = (catMaybes 
 					   (map palindrome 
-					   	 (map T.unpack 
+					   	 (generateCandidates x)))
+
+generateCandidates :: String -> [String]
+generateCandidates x = (map T.unpack 
 					   	   (T.tails 
-					   	   	 (T.pack x)))))) -1 + (length x)
+					   	   	 (T.pack x)))
 
 
 -- map palindrome (map T.unpack (T.tails (T.pack "12345")))

38fe080f1a6bbf27350095f7f3fd73c1348d5d9d
Alvaro Garcia <derecerca@gmail.com>
2015-08-19 20:46:38 +0200

    refactor: remove dead code

--- palindrome.hs
+++ palindrome.hs
@@ -26,16 +26,3 @@ substringIndices x = [[i,j] | i<- [0.. length x], j<-[i..(length x) - 1]]
 substring :: [Int] -> T.Text -> T.Text
 substring [start, end] text =  T.take (end - start + 1 ) $ T.drop start $ text
 
-
-
--- map palindrome (map T.unpack (T.tails (T.pack "12345")))
--- [Nothing,Nothing,Nothing,Nothing,Just "5",Nothing]
-
--- import Data.Maybe
--- filter isJust [Nothing,Nothing,Nothing,Nothing,Just "5",Nothing]
--- [Just "5"]
-
-
--- *Main T Data.Maybe> (filter isJust (map palindrome (map T.unpack (T.tails (T.pack "ana")))))
-
-

4fa3a42adfa409a5ef19ec10e0aedf03f5c512fb
Alvaro Garcia <derecerca@gmail.com>
2015-08-19 20:08:37 +0200

    red: add function that count palindromes
    but has defects:
      * anaana = 8 (should be 9)
    
    OK:
     * ana = 4

--- palindrome.hs
+++ palindrome.hs
@@ -1,5 +1,29 @@
+import qualified Data.Text as T
+import Data.Maybe
+
 palindrome :: String -> Maybe String
 palindrome [] = Nothing
 palindrome x = if x == (reverse x)
 			   then Just x
 			   else Nothing
+
+countPalindromes :: String -> Int
+countPalindromes x = (length 
+					 (filter isJust 
+					   (map palindrome 
+					   	 (map T.unpack 
+					   	   (T.tails 
+					   	   	 (T.pack x)))))) -1 + (length x)
+
+
+-- map palindrome (map T.unpack (T.tails (T.pack "12345")))
+-- [Nothing,Nothing,Nothing,Nothing,Just "5",Nothing]
+
+-- import Data.Maybe
+-- filter isJust [Nothing,Nothing,Nothing,Nothing,Just "5",Nothing]
+-- [Just "5"]
+
+
+-- *Main T Data.Maybe> (filter isJust (map palindrome (map T.unpack (T.tails (T.pack "ana")))))
+
+
